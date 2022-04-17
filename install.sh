5ccc668a05523a0baff90d65a2b1b8761ebec4ce
MavPig <mavcode.p@gmail.com>
2022-04-17 22:52:56 +0800

    source pip conda in main install.sh

--- install.sh
+++ install.sh
@@ -2,5 +2,16 @@
 
 SCRIPT_DIR=`dirname "$(readlink -f "$0")"`
 
+echo ==================== Neovim =========================
 source $SCRIPT_DIR/nvim/install.sh
+echo ==================== Neovim Done ====================
+
+echo ==================== Conda ==========================
+source $SCRIPT_DIR/miniconda/install.sh
+echo ==================== Conda Done =====================
+
+
+echo ==================== pip ============================
+source $SCRIPT_DIR/pip/install.sh
+echo ==================== pip Done =======================
 
