2dabbe6565f1991e64945e8dc342578b310989c8
MavPig <mavcode.p@gmail.com>
2022-04-17 22:15:03 +0800

    add miniconda

---
+++ miniconda/install_conda.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+SCRIPT_DIR=`dirname "$(readlink -f "$0")"`
+
+if [[ ! -e ~/miniconda3 ]]
+then
+	MINICONDA_INSTALL_SH="${SCRIPT_DIR}/Miniconda3-latest-Linux-x86_64.sh"
+	if [[ ! -e $MINICONDA_INSTALL_SH ]]; then
+		wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O $MINICONDA_INSTALL_SH
+	fi
+	bash $MINICONDA_INSTALL_SH
+	rm $MINICONDA_INSTALL_SH
+fi
+
+# copy .condarc to ~
+if [[ ! -e ~/.condarc ]]
+then
+	cp ${SCRIPT_DIR}/.condarc ~
+fi
+
+# install conda envs
+cat ${SCRIPT_DIR}/envs/envs.txt | while read env; do 
+echo $env
+if [[ -z `conda env list | grep ${env}` ]]
+then
+	echo "conda env create -f=${SCRIPT_DIR}/envs/${env}.yml -n ${env}"
+	conda env create -f=${SCRIPT_DIR}/envs/${env}.yml -n ${env}
+fi
+done
+
